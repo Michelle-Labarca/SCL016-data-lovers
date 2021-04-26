a3388724329e13fb940387c10fa02bed0d010105
Michelle <labarcamoralesm@gmail.com>
2021-04-26 04:24:50 -0400

    Se rehace lógica de mostrar pokemon, se añade filtro por nombre, se añade funcion de mostrar modal.

--- src/data.js
+++ src/data.js
@@ -1,4 +1,14 @@
 
+export const filtrarPorNombre = (allPokemon,nombre) => {
+  let pokemonFiltrados = allPokemon;
+  if (nombre != "") {
+    pokemonFiltrados = allPokemon.filter(
+      pokemon => pokemon.name.indexOf(nombre) > -1,
+    );
+  }
+  return pokemonFiltrados;
+}
+
 //Filtra a todos los pokemones por su tipo
 export const filtrarPorTipo = (allPokemon,type) => {
   let pokemonFiltrados = allPokemon;
@@ -15,6 +25,16 @@ export const filtrarPorDebilidad = (allPokemon,debilidad) => {
   if (debilidad != "all") {
     pokemonFiltrados = allPokemon.filter(
       pokemon => pokemon.weaknesses.indexOf(debilidad) > -1,
+    );
+  }
+  return pokemonFiltrados;
+};
+
+export const filtrarPorResistencia = (allPokemon,resistencia) => {
+  let pokemonFiltrados = allPokemon;
+  if (resistencia != "all") {
+    pokemonFiltrados = allPokemon.filter(
+      pokemon => pokemon.resistant.indexOf(resistencia) > -1,
     );
   }
   return pokemonFiltrados;
--- src/main.js
+++ src/main.js
@@ -1,51 +1,145 @@
-import { filtrarPorTipo, filtrarPorDebilidad } from './data.js';
+import { filtrarPorTipo, filtrarPorDebilidad, filtrarPorResistencia, filtrarPorNombre } from './data.js';
 import data from "./data/pokemon/pokemon.js";
 
-//Obtenemos el elemento poke-Container desde html
 const pokeContainer = document.getElementById("poke-Container");
 
-//Creamos la funcion que crea las Box
-function createPokemonBox(poke) {
+const obtenerPokemon = (data) => {
+    const pokemon = data.map((result) => ({
+        name: result.name[0].toUpperCase() + result.name.slice(1),
+        img: result.img,
+        num: result.num
+    }));
+    displayPokemon(pokemon);
+};
 
-    //Dentro de mi pokemonBox crea una section con class="poke"
-	const pokemonBox = document.createElement("section");
-	pokemonBox.classList.add("poke");
-
-    //Declarando constantes para los datos de mi pokemonBox
-    const img = poke.img;
-    // Pasamos a mayúscula la primera letra y pegamos el resto del nombre
-	const name = poke.name[0].toUpperCase() + poke.name.slice(1);
-	const number = poke.num;
-
-    //Creamos una constante y dentro de ella guardamos el codigo html a 
-    //insertar en pokemonBox
-	const pokeInnerHTML = `
+const displayPokemon = (pokemon) => {
+    const pokemonHtmlInnert = pokemon.map(
+        (poke) => `
+        <section class="poke">
         <section class="img-container">
-            <img src="${img}" />        
+            <img src="${poke.img}" />        
         </section>
         <section class="info">
-            <p class="name">${name}</p>
-            <p class="number">#${number}</p>
+            <p class="name">${poke.name}</p>
+            <p class="number">#${poke.num}</p>
         </section>
-    `;
-    //Añadimos en la pokemonBox el codigo html creado anteriormente (pokeInnerHtml)
-	pokemonBox.innerHTML = pokeInnerHTML;
-    //Añade este pokemonBox como hijo de este pokeContainer
-    pokeContainer.appendChild(pokemonBox);
-}
+        </section>
+    `
+    )
+    .join("");
+    pokeContainer.innerHTML = pokemonHtmlInnert + pokeContainer.innerHTML;
+};
 
-// -------------------------------------------------
+obtenerPokemon(data.pokemon);
 
-//Creo la funcion para llenar con los pokemon las pokemonBox
-function fillContainer(dataPoke){
-    //Por cada pokemon de la data voy a crear su propia box
-        dataPoke.forEach(pokeSelect => {
-            createPokemonBox(pokeSelect);
+// --------------------------- TARJETA POKEMON -----------------------------
+//Obtenemos todos los hijos del nodo pokeContainer (childCard)
+const allPokeCard = document.getElementById("poke-Container").childNodes;
+allPokeCard.forEach(function(childCard, i) {
+    if (i%2 == 1) {
+        //Obtenemos la data del pokemon correspondiente a la respectiva tarjeta
+        const infoPokemon = data.pokemon[(i-1)/2];
+        childCard.addEventListener("click",() =>{
+            showModal(infoPokemon);
+            const modal = document.querySelector(".modal");
+            const closeBtn = document.querySelector(".closeBtn");
+            closeBtn.addEventListener("click", () => {
+                modal.parentElement.removeChild(modal);
+                obtenerPokemon(data.pokemon);
+            });
         })
     }
+});
 
-//llamo a la funcion de fillContainer y le paso la data de los pokemon
-fillContainer(data.pokemon);
+const showModal = (infoPokemon) => {
+    const name = infoPokemon.name[0].toUpperCase() + infoPokemon.name.slice(1);
+    const num = infoPokemon.num;
+    const img = infoPokemon.img;
+    const generationName = infoPokemon.generation.name.toUpperCase();
+    const generationNum = infoPokemon.generation.num.toUpperCase();
+    const height = infoPokemon.size.height;
+    const weigth = infoPokemon.size.weight;
+    const type = infoPokemon.type;
+    const weaknesses = infoPokemon.weaknesses;
+    const description = infoPokemon.about;
+    const attackPoke = infoPokemon.stats["base-attack"];
+
+    const htmlCard = ` 
+        <section class="modal">
+            <main class="pokemon-card">
+            <section class="close">
+                <button class="closeBtn">X</button>
+            </section>
+            <section class="title-row">
+                <p class="generation">${generationName}</p>
+                <h1 class="name">${name} n°${num}</h1>
+            </section>
+
+            <section class="character-img">
+                <img src=${img}>
+            </section>
+
+            <section class="character-meta">
+                <p>${generationNum} | HEIGHT: ${height} | WEIGHT: ${weigth}</p>
+            </section>
+            
+            <section class="character-info">
+                <p><b>Type: </b>${type.join("/")}</p>
+                <p><b>Weaknesses: </b>${weaknesses.join("/")}</p>
+            </section>
+
+            <section class="character-description">
+                <p><i>${description}</i></p>
+            </section>
+
+            <section class="character-attack">
+                <label for="attack">Attack</label>
+                <progress id="attack" value=${attackPoke} max="300"></progress>
+                ${attackPoke}
+            </section>
+
+            <main>
+
+        </section>
+    `;
+    pokeContainer.innerHTML = htmlCard;
+}
+
+//--------------------------------FILTROS-----------------------------------------
+
+//Creo una constante para guardar el input del nombre
+const nameFilter = document.getElementById("nameFilter");
+// Añado un evento para cuando se teclee algo
+nameFilter.addEventListener("keypress", (e) => {
+    // Si la tecla que se presiona es 'enter', realizo la búsqueda por el nombre ingresado
+    if (e.key === 'Enter') {
+        const node = document.getElementById("poke-Container");
+        node.innerHTML = "";
+        // Obtengo del input el valor ingresado
+        const name = nameFilter.value;
+        let pokemones_filtrados = filtrarPorNombre(data.pokemon,name);
+        //Por cada pokemon filtrado vamos a crear su respectiva box
+        obtenerPokemon(pokemones_filtrados);
+        //Obtenemos todos los hijos del nodo pokeContainer (childCard)
+        const allPokeCard = document.getElementById("poke-Container").childNodes;
+        allPokeCard.forEach(function(childCard, i) {
+            if (i%2 == 1) {
+                //Obtenemos la data del pokemon correspondiente a la respectiva tarjeta
+                const infoPokemon = pokemones_filtrados[(i-1)/2];
+                childCard.addEventListener("click",() =>{
+                    showModal(infoPokemon);
+                    const modal = document.querySelector(".modal");
+                    const closeBtn = document.querySelector(".closeBtn");
+                    closeBtn.addEventListener("click", () => {
+                        modal.parentElement.removeChild(modal);
+                        obtenerPokemon(data.pokemon);
+                    });
+                })
+            }
+        });
+      }
+});
+
 
 //Creo una constante para para guardar el selector de tipos
 const typeSelect = document.getElementById("type");
@@ -59,8 +153,23 @@ typeSelect.addEventListener("change", () => {
     //Obtenemos la lista de pokemones filtrados de acuerdo al valor seleccionado
     let pokemones_filtrados = filtrarPorTipo(data.pokemon,tipo);
     //Por cada pokemon filtrado vamos a crear su respectiva box
-    pokemones_filtrados.forEach(pokeSelect => {
-        createPokemonBox(pokeSelect);
+    obtenerPokemon(pokemones_filtrados);
+    //Obtenemos todos los hijos del nodo pokeContainer (childCard)
+    const allPokeCard = document.getElementById("poke-Container").childNodes;
+    allPokeCard.forEach(function(childCard, i) {
+        if (i%2 == 1) {
+            //Obtenemos la data del pokemon correspondiente a la respectiva tarjeta
+            const infoPokemon = pokemones_filtrados[(i-1)/2];
+            childCard.addEventListener("click",() =>{
+                showModal(infoPokemon);
+                const modal = document.querySelector(".modal");
+                const closeBtn = document.querySelector(".closeBtn");
+                closeBtn.addEventListener("click", () => {
+                    modal.parentElement.removeChild(modal);
+                    obtenerPokemon(data.pokemon);
+                });
+            })
+        }
     });
 });
 
@@ -70,23 +179,48 @@ weaknessesSelect.addEventListener("change", () => {
     node.innerHTML = "";
     const debilidad = weaknessesSelect.value;
     let pokemones_filtrados = filtrarPorDebilidad(data.pokemon,debilidad);
-    pokemones_filtrados.forEach(pokeSelect => {
-        createPokemonBox(pokeSelect);
+    obtenerPokemon(pokemones_filtrados);
+    //Obtenemos todos los hijos del nodo pokeContainer (childCard)
+    const allPokeCard = document.getElementById("poke-Container").childNodes;
+    allPokeCard.forEach(function(childCard, i) {
+        if (i%2 == 1) {
+            //Obtenemos la data del pokemon correspondiente a la respectiva tarjeta
+            const infoPokemon = pokemones_filtrados[(i-1)/2];
+            childCard.addEventListener("click",() =>{
+                showModal(infoPokemon);
+                const modal = document.querySelector(".modal");
+                const closeBtn = document.querySelector(".closeBtn");
+                closeBtn.addEventListener("click", () => {
+                    modal.parentElement.removeChild(modal);
+                    obtenerPokemon(data.pokemon);
+                });
+            })
+        }
     });
 });
 
-/* Testeo busqueda por nombre
-const pokemonTest = test(data.pokemon,"pik");
-console.log(pokemonTest);
-*/
-
-/*Funcion tarjeta pokemon
-
-const allPokeCard = document.getElementById("poke-Container").childNodes;
-allPokeCard.forEach(function(childCard, i) {
-    //console.log(card);
-    childCard.onclick = function() {
-    // eslint-disable-next-line no-console
-    console.log(data.pokemon[i-1]);
-
-}});*/
+const resistantSelect = document.getElementById("resistant");
+resistantSelect.addEventListener("change", () => {
+    const node = document.getElementById("poke-Container");
+    node.innerHTML = "";
+    const resistencia = resistantSelect.value;
+    let pokemones_filtrados = filtrarPorResistencia(data.pokemon,resistencia);
+    obtenerPokemon(pokemones_filtrados);
+    //Obtenemos todos los hijos del nodo pokeContainer (childCard)
+    const allPokeCard = document.getElementById("poke-Container").childNodes;
+    allPokeCard.forEach(function(childCard, i) {
+        if (i%2 == 1) {
+            //Obtenemos la data del pokemon correspondiente a la respectiva tarjeta
+            const infoPokemon = pokemones_filtrados[(i-1)/2];
+            childCard.addEventListener("click",() =>{
+                showModal(infoPokemon);
+                const modal = document.querySelector(".modal");
+                const closeBtn = document.querySelector(".closeBtn");
+                closeBtn.addEventListener("click", () => {
+                    modal.parentElement.removeChild(modal);
+                    obtenerPokemon(data.pokemon);
+                });
+            })
+        }
+    });
+});
